346090245ec18cdbde11a22595e271cb3a3ce26a
StanleyBack-dev <stanleyrodrigues504@gmail.com>
2026-08-16 15:19:35 -0300

    style: fix prettier formatting in pix authorization id migration

--- src/database/migrations/1787400000000-AddGatewayPixAuthorizationIdToSubscriptions.ts
+++ src/database/migrations/1787400000000-AddGatewayPixAuthorizationIdToSubscriptions.ts
@@ -1,8 +1,6 @@
 import type { MigrationInterface, QueryRunner } from "typeorm";
 
-export class AddGatewayPixAuthorizationIdToSubscriptions1787400000000
-  implements MigrationInterface
-{
+export class AddGatewayPixAuthorizationIdToSubscriptions1787400000000 implements MigrationInterface {
   name = "AddGatewayPixAuthorizationIdToSubscriptions1787400000000";
 
   public async up(queryRunner: QueryRunner): Promise<void> {
